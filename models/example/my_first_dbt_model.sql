ec72c90c398035f13d7fef40dff5a9e568126893
charles.verleyen@richemont.com <charles.verleyen@richemont.com>
2021-06-28 14:50:25 +0000

    second commit

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -1,4 +1,7 @@
 
+-- SELECT * 
+-- FROM bqdts_sbcharles1_datalake_playground.customers
+
 /*
     Welcome to your first dbt model!
     Did you know that you can also configure models directly within SQL files?
